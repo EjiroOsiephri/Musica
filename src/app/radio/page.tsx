c0ec2c4545fca45cce2d876374e18df2c06de13f
ejiro osiephri <osiephriejiro765@gmail.com>
2025-03-28 22:58:19 +0100

    feats/add added authentication route

--- src/app/radio/page.tsx
+++ src/app/radio/page.tsx
@@ -1,6 +1,10 @@
 import PodcastComponent from "../../components/PodcastsComponent";
+import useAuth from "@/hooks/useAuth";
 
 export default function Radio() {
+  useAuth();
+  // useAuth is called to ensure that the user is authenticated before rendering the component
+
   return (
     <>
       <PodcastComponent />
--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,6 +1,10 @@
 import MusicVideoComponent from "@/components/MusicVideoComponent";
+import useAuth from "@/hooks/useAuth";
 
 export default function Video() {
+  useAuth();
+
+  // useAuth is called to ensure that the user is authenticated before rendering the component
   return (
     <>
       <MusicVideoComponent />
